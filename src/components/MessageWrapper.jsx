0d5a1ae285d0f5f47a3d47c2edc049cc7e69cba7
Niklas Eliason <niklas.eliason@decisionbyheart.com>
2023-12-14 09:27:54 +0100

    add background color to messages

--- src/components/MessageWrapper.jsx
+++ src/components/MessageWrapper.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const MessageWrapper = ({ message }) => (
-	<div className="py-10 mb-10 text-xl text-center border border-gray-100 rounded-md shadow-md text-primary-500 px-14">
+	<div className="py-10 mb-10 text-xl text-center bg-white border border-gray-100 rounded-md shadow-md text-primary-500 px-14">
 		{message}
 	</div>
 );
