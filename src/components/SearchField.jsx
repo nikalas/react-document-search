bf8d9fd4de824641e2ea3710c95552ec37a9c83a
Niklas Eliason <niklas.eliason@decisionbyheart.com>
2023-12-14 09:34:55 +0100

    add debounce to search input

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,5 +1,6 @@
 import useTranslations from "./translations"; // TODO: This can be removed once proper i18n translations are working
 import { fieldClasses } from "./DocumentSearch";
+import { DebounceInput } from "react-debounce-input";
 
 export const SearchField = ({ search, setSearch, className = "" }) => {
 	const __ = useTranslations(); // TODO: This can be removed once proper i18n translations are working
@@ -10,7 +11,9 @@ export const SearchField = ({ search, setSearch, className = "" }) => {
 				{__("Search", "dbyh")}:
 			</header>
 
-			<input
+			<DebounceInput
+				minLength={3}
+				debounceTimeout={300}
 				className={fieldClasses}
 				type="text"
 				value={search}
